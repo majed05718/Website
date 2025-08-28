c1bfce7652e68b1eeb00b7a1d5849eda8b25ddfc
majed05718 <dr.majed05718@gmail.com>
2025-08-28 21:42:32 +0300

    Create sentry.client.config.ts

---
+++ Web/sentry.client.config.ts
@@ -0,0 +1,37 @@
+// web/sentry.client.config.ts
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.NEXT_PUBLIC_ENV || 'development',
+  
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+  
+  replaysOnErrorSampleRate: 1.0,
+  replaysSessionSampleRate: 0.1,
+  
+  integrations: [
+    new Sentry.Replay({
+      maskAllText: true,
+      blockAllMedia: true,
+    }),
+  ],
+});
+
+// web/sentry.server.config.ts
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.NEXT_PUBLIC_ENV || 'development',
+  
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1.0,
+  
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
