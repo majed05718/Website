e70f13f2d6a8d26292b051d0cae07787226c2524
majed05718 <dr.majed05718@gmail.com>
2025-08-28 07:58:40 +0300

    Create health.module.ts

---
+++ Web/api/src/health/health.module.ts
@@ -0,0 +1,8 @@
+// api/src/health/health.module.ts
+import { Module } from '@nestjs/common';
+import { HealthController } from './health.controller';
+
+@Module({
+  controllers: [HealthController],
+})
+export class HealthModule {}
