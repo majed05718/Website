9eb86268a17981fa7f5052de0f1a821d7feabc58
majed05718 <dr.majed05718@gmail.com>
2025-08-28 22:47:39 +0300

    Update index.tsx

--- Web/src/providers/index.tsx
+++ Web/src/providers/index.tsx
@@ -1,5 +1,4 @@
-
-
+'use client';
 import { createContext, useContext } from 'react';
 import { SupabaseProvider } from './supabase-provider';
 import { PostHogContextProvider } from './posthog-provider';
