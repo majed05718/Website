abe30aed42a4309f6a0304b0639fd1cd166564a0
majed05718 <dr.majed05718@gmail.com>
2025-08-28 07:40:57 +0300

    Create app.module.ts

---
+++ Web/api/src/app.module.ts
@@ -0,0 +1,20 @@
+// api/src/app.module.ts
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { HealthModule } from './health/health.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    ThrottlerModule.forRoot([{
+      ttl: 60000, // 1 minute
+      limit: 100, // requests per TTL
+    }]),
+    HealthModule,
+  ],
+})
+export class AppModule {}
